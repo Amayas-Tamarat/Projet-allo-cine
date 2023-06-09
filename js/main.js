b99d9e10725d3ca7d6469bb422b5101e863cc7dd
Amayas-Tamarat <amayas.tamarat@gmail.com>
2023-06-09 14:31:32 +0200

    list title js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-fetch("./data/moovies.js")
+let title = []
+let plot
+let poster
+let meta
+let imdb
+fetch("./data/moovies.json")
 .then(response => response.json())
 .then(data => {
     for (var i = 0; i<data.length; i++){
-        let title = data[i].Title;
-        let actor = data[i].Actors;
-        let poster = data[i].Poster
+    /* title = data[i].Title;
+        plot = data[i].Plot;
+        poster = data[i].Poster;
+        meta = data[i].Metascore;
+        imdb = data[i].imdbRating;
+        */
+       title.push (data[i].Title)
     }
     console.log(title);
 }
 )
+
+function gettitle(){
+    return title
+}
